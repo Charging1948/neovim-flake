a4bd7619281ed6ff6cac8e5ef2b5ac14a9580618
jk <joachim.dev@pm.me>
2024-01-24 15:43:41 +0100

    add justfile, .envrc and adjust menu of alpha.nix

--- config/alpha.nix
+++ config/alpha.nix
@@ -34,9 +34,37 @@
             shortcut = "e";
           }
           {
+            type = "button";
+            val = "  Find text";
+            on_press.raw = "require('telescope.builtin').live_grep";
+
+            opts = {
+              keymap = ["n" "g" ":Telescope live_grep <CR>" {noremap = true; silent = true; nowait = true;}];
+              shortcut = "g";
+            };
+          }
+          {
             command = ":qa<CR>";
             desc = "Quit Neovim";
             shortcut = "<leader>q";
+          }
+          {
+            type = "button";
+            val = "<DESC>";
+
+            on_press.raw = "funcion() vim.cmd[[:qa<CR>]] end";
+
+            opts = {
+              keymap = ["n" "<SC>" "<COMMAND>" {noremap = true; silent = true; nowait = true;}];
+              shortcut = "<SC>";
+
+              # Feel free to customize (or omit) those
+              # position = "center";
+              # cursor = 3;
+              # width = 50;
+              # align_shortcut = "right";
+              # hl_shortcut = "Keyword";
+            };
           }
         ];
       }
