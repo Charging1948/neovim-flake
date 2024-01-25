2d968c281a72df779c5a4c0293f4a1269d512c8b
jk <joachim.dev@pm.me>
2024-01-25 15:44:20 +0100

    add sources to nvim-cmp and mappings

--- config/completion/nvim-cmp.nix
+++ config/completion/nvim-cmp.nix
@@ -9,5 +9,35 @@
       panel.enabled = false;
       suggestion.enabled = false;
     };
+    nvim-cmp = {
+      mapping = {
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-e>" = "cmp.mapping.close()";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<S-Tab>" = {
+          action = "cmp.mapping.select_prev_item()";
+          modes = [
+            "i"
+            "s"
+          ];
+        };
+        "<Tab>" = {
+          action = "cmp.mapping.select_next_item()";
+          modes = [
+            "i"
+            "s"
+          ];
+        };
+      };
+      snippet.expand = "luasnip";
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "luasnip"; } #For luasnip users.
+        { name = "path"; }
+        { name = "buffer"; }
+      ];
+    };
   };
 }
